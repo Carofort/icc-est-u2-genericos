a37d004c555d1e0c47ca42a3bd5382d36c0842b4
Fortmann <dayannafort@hmail.com>
2024-12-02 11:53:32 -0500

    Segundo Commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,24 @@
+import models.Persona;
+import models.genericos.Caja;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Caja<String> cajaString = new Caja<>();
+        Caja<Integer> cajaInteger = new Caja<>();
+        Caja<Persona> cajaPersona = new Caja<>();
+
+        Persona persona = new Persona ();
+        persona.setNombre("Carolina");
+        persona.setEdad(20);
+
+        cajaString.guardar("Guardo un String");
+        cajaInteger.guardar(10);
+        cajaPersona.guardar(persona);
+
+        System.out.println(cajaString.obtener());
+        System.out.println(cajaInteger.obtener());
+        System.out.println(cajaPersona.obtener());
+
     }
 }
+
